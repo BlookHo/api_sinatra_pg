a86ea52ac9e5fc7d2caa917a6d71d33eb9bcc4e5
Aleksei <blookho@users.noreply.github.com>
2017-05-10 16:50:24 +0300

    API start /

--- api_sinatra_postgres.rb
+++ api_sinatra_postgres.rb
@@ -48,8 +48,8 @@ end
 
 get '/' do
   # system 'rake --tasks'
-  # "Hi! I am api_sinatra_postgres!\n\n"
-  haml :index, :layout => :my_layout
+  "Hi! I am api_sinatra_postgres!\n\n"
+  # haml :index, :layout => :my_layout
 end
 
 
